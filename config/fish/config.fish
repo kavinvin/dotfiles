b35909b1f65ba16b06179dcb83bdd6a78b21b547
kavinvin <kavinvin.vin@gmail.com>
2019-01-21 00:04:32 +0700

    Reindent files

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,25 +1,26 @@
 function add_path
-  set -gx PATH $argv[1] $PATH
+    set -gx PATH $argv[1] $PATH
 end
 
 function vf
-  if test not $is_vf_loaded
-    set -gx is_vf_loaded true
-    echo 'Loading virtualfish...'
-    eval (python3 -m virtualfish)
-  end
-  vf $argv
+    if test not $is_vf_loaded
+        set -gx is_vf_loaded true
+        echo 'Loading virtualfish...'
+        eval (python3 -m virtualfish)
+    end
+    vf $argv
 end
 
 function set_path
+    add_path $HOME/.jenv/bin
 end
 
 function load
-  set_path
-  fish_vi_key_bindings
-  source $HOME/.config/fish/custom/alias.fish
-  source $HOME/.config/fish/custom/utils.fish
-  source $HOME/.config/fish/custom/functions.fish
+    set_path
+    fish_vi_key_bindings
+    source $HOME/.config/fish/custom/alias.fish
+    source $HOME/.config/fish/custom/utils.fish
+    source $HOME/.config/fish/custom/functions.fish
 end
 
 load
