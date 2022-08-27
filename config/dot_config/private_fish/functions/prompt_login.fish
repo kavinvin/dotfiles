c6bfe20434beaa599115ff750ca9f04f5bc37e99
kavinvin <kavinvin.vin@gmail.com>
2022-08-27 21:57:41 +0700

    Hide machine name if not in ssh session

---
+++ config/dot_config/private_fish/functions/prompt_login.fish
@@ -0,0 +1,38 @@
+# Defined in /opt/homebrew/Cellar/fish/3.5.1/share/fish/functions/prompt_login.fish @ line 1
+function prompt_login --description 'display user name for the prompt'
+    if not set -q __fish_machine
+        set -g __fish_machine
+        set -l debian_chroot $debian_chroot
+
+        if test -r /etc/debian_chroot
+            set debian_chroot (cat /etc/debian_chroot)
+        end
+
+        if set -q debian_chroot[1]
+            and test -n "$debian_chroot"
+            set -g __fish_machine "(chroot:$debian_chroot)"
+        end
+    end
+
+    # Prepend the chroot environment if present
+    if set -q __fish_machine[1]
+        echo -n -s (set_color yellow) "$__fish_machine" (set_color normal) ' '
+    end
+
+    function machine --no-scope-shadowing
+        # If we're running via SSH, change the host color.
+        set -l color_host $fish_color_host
+        if set -q SSH_TTY; and set -q fish_color_host_remote
+            set color_host $fish_color_host_remote
+        end
+
+        if test $SSH_CLIENT
+            echo -n -s @ (set_color $color_host) (prompt_hostname) (set_color normal)
+	      else
+	          echo -n -s ""
+        end
+    end
+
+    echo -n -s (set_color $fish_color_user) "$USER" (set_color normal) (machine)
+end
+
