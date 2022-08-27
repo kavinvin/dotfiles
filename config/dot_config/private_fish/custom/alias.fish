f22dc229852c8297c9b4f18e7bc1f1fc471a128f
kavinvin <kavinvin.vin@gmail.com>
2022-08-27 20:10:16 +0700

    Remove unused alias

--- config/dot_config/private_fish/custom/alias.fish
+++ config/dot_config/private_fish/custom/alias.fish
@@ -26,32 +26,10 @@ abbr o 'open .'
 abbr p ping
 abbr p1 'ping 1.1.1.1'
 abbr p8 'ping 8.8.8.8'
-abbr pc 'pbcopy'
-abbr pp pbpaste
-abbr pe pipenv
-alias please sudo
-abbr pt 'ptpython'
-abbr pu pushd
 abbr python python3
 abbr pip 'python3 -m pip'
-abbr py python3
-abbr rd rmdir
-alias safari 'open -a safari'
-abbr ssl openssl
-abbr ta 'tmux a -t'
-abbr tn 'tmux new -s'
-abbr kat 'kafkacat'
-abbr ne nix-env
-abbr k kanji
 alias e exit
-abbr race typerace
-alias reload 'source $HOME/.config/fish/config.fish'
-abbr ts ts-node
-abbr k kubectl
-abbr gssh 'gcloud compute ssh'
 alias vim nvim
 alias vi nvim
-alias v 'f -e nvim'
-abbr gfuck 'git add --all; git commit --amend --no-edit; git push -f'
-alias dsmlr 'ssh -J dsmlr.kavin.io'
-alias kplus 'ssh -J 167.99.68.183:9022'
+
+alias ghci 'stack exec -- ghci'
