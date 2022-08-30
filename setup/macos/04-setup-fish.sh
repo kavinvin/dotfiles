bf2c7124074355bd8581e74308fe5300d1275e9c
kavinvin <kavinvin.vin@gmail.com>
2022-08-30 15:20:46 +0700

    Check Fisher packages

--- setup/macos/04-setup-fish.sh
+++ setup/macos/04-setup-fish.sh
@@ -11,7 +11,7 @@ fi
 
 # Install Fisher
 # https://github.com/jorgebucaran/fisher#installation
-if test -f ~/.config/fish/functions/fisher.fish
+if test -f $HOME/.config/fish/functions/fisher.fish
 then
   echo "Fisher is already installed."
 else
@@ -19,11 +19,20 @@ else
 fi
 
 # Install Fisher packages
-fish -c "fisher install catppuccin/fish"
-fish -c "fisher install jhillyerd/plugin-git"
-fish -c "fisher install jethrokuan/z"
-fish -c "fisher install franciscolourenco/done"
-fish -c "fisher install jorgebucaran/spark.fish"
+function fisher-install {
+  PACKAGE="$1"
+  if test -f $HOME/.config/fish/fish_plugins && grep -Fxq $PACKAGE $HOME/.config/fish/fish_plugins
+  then
+    echo "Fisher package $PACKAGE is already installed."
+  else
+    fish -c "fisher install $PACKAGE"
+  fi
+}
+fisher-install catppuccin/fish
+fisher-install jhillyerd/plugin-git
+fisher-install jethrokuan/z
+fisher-install franciscolourenco/done
+fisher-install jorgebucaran/spark.fish
 
 # Disable fish greeting message
 fish -c "set -U fish_greeting ''"
