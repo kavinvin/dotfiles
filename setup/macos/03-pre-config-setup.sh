4136ebde9beb5c1466f821b2680e06a78c282dcf
kavinvin <kavinvin.vin@gmail.com>
2022-08-30 20:04:26 +0700

    Add NvChad installation as external archive

--- setup/macos/03-pre-config-setup.sh
+++ setup/macos/03-pre-config-setup.sh
@@ -8,14 +8,6 @@ else
   $(brew --prefix)/opt/fzf/install
 fi
 
-# Install NvChad repo as neovim config
-if test -f $HOME/.config/nvim/.git/config && grep -Fq "NvChad" $HOME/.config/nvim/.git/config
-then
-  echo "NvChad is already installed."
-else
-  git clone https://github.com/NvChad/NvChad $HOME/.config/nvim --depth 1 && nvim
-fi
-
 # Install latest node version
 if command -v node &> /dev/null
 then
