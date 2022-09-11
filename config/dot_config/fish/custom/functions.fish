cc61343d10500e87d41ec6d8602048a68dd782aa
kavinvin <kavinvin.vin@gmail.com>
2022-09-11 16:00:11 +0700

    Remove unused functions

--- config/dot_config/fish/custom/functions.fish
+++ config/dot_config/fish/custom/functions.fish
@@ -1,46 +1,11 @@
 function chrome
-	open -a "Google Chrome" $argv[0]
+	open -a "Google Chrome" $argv
 end
 
 function docker-ssh
 	docker exec -it $argv[1] /bin/sh
 end
 
-function mc
-	mkdir -p $argv[1]; cd $argv[1]
-end
-
-function sudo
-	if test "$argv" = !!
-        eval command sudo $history[1]
-    else
-        command sudo $argv
-    end
-end
-
-function pandoc-preview-md
-	mkdir -p /tmp/pandoc
-    set filename $argv[1]
-    set temp_preview
-    pandoc -f markdown -t html < $filename > /tmp/pandoc/$filename.html
-    safari /tmp/pandoc/$filename.html
-end
-
-function th
-    sdcv $argv[1] -u 'English-Thai Lexitron Dictionary 2.0' -c | less -r
-end
-function en
-    sdcv $argv[1] -u 'dictd_www.dict.org_gcide' -c | less -r
-end
-function je
-    sdcv $argv[1] -u 'jmdict-ja-en' -c
-end
-function ej
-    sdcv $argv[1] -u 'jmdict-en-ja' -c
-end
 function fish-time
     fish --profile /tmp/profile -c fish_prompt; cat /tmp/profile
-end
-function hk
-    ssh HKG+kruengpratee-hkg@$argv[1]
 end
