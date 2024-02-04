1186b99efc2954db5cc18dab527bb0207aa8affa
kavinvin <kavinvin.vin@gmail.com>
2024-02-04 15:57:39 +0900

    Don't run app store installation list if on CI

--- setup/macos/06-mac-app-store.sh
+++ setup/macos/06-mac-app-store.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-mas install 747648890 # Telegram
-mas install 803453959 # Slack
-mas install 1569813296 # 1Password for Safari
-mas install 1482454543 # Twitter
-mas install 1480068668 # Messenger
+if [ "$CI" != "true" ]; then
+  mas install 747648890 # Telegram
+  mas install 803453959 # Slack
+  mas install 1569813296 # 1Password for Safari
+  mas install 1482454543 # Twitter
+  mas install 1480068668 # Messenger
+fi
