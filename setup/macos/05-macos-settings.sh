5fce8ee790deb700d0575296c5d21c77615d10d0
Kavin Ruengprateepsang <kavinvin.vin@gmail.com>
2024-04-02 02:51:28 +0900

    Update 05-macos-settings.sh

--- setup/macos/05-macos-settings.sh
+++ setup/macos/05-macos-settings.sh
@@ -5,3 +5,9 @@ defaults write -g AppleLanguages -array en th ja
 
 # Disable show recents section in the dock
 defaults write com.apple.dock "show-recents" -bool false
+
+# Specify iTerm2 preferences directory
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "~/.config/iterm2"
+
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
