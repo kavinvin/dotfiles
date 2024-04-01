d9fd58d2248cf648e81d82ec7382ba3c3bd404cc
Kavin Ruengprateepsang <kavinvin.vin@gmail.com>
2024-04-02 02:37:34 +0900

    Update 06-mac-app-store.sh

--- setup/macos/06-mac-app-store.sh
+++ setup/macos/06-mac-app-store.sh
@@ -3,9 +3,14 @@
 export PATH="$PATH:/opt/homebrew/bin"
 
 if [ "$CI" != "true" ]; then
-  mas install 747648890 # Telegram
-  mas install 803453959 # Slack
-  mas install 1569813296 # 1Password for Safari
-  mas install 1482454543 # Twitter
-  mas install 1480068668 # Messenger
+  read -p "Do you want to install App Store apps? (y/n)" -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    mas install 747648890 # Telegram
+    mas install 803453959 # Slack
+    mas install 1569813296 # 1Password for Safari
+    mas install 1482454543 # Twitter
+    mas install 1480068668 # Messenger
+  fi
 fi
